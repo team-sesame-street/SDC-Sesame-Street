dae3454673074c4a9c593b1cfacf31758f47c9f2
Gian Franco Lazaro <gianlazaro7411@gmail.com>
2022-07-08 13:17:45 -0700

    Created wrapper for all question and answer subcomponents

--- client/src/components/questionsAnswers/QaBox.jsx
+++ client/src/components/questionsAnswers/QaBox.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import QaListItem from './QaListItem.jsx';
 
-function QaBox() {
+function QaBox({ id }) {
+  console.log(id);
+  const [currProduct, setCurrProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    if (id) {
+      axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions?product_id=${id}`, {
+        headers: {
+          Authorization: process.env.GITKEY,
+        },
+      })
+        .then(({ data }) => {
+          setCurrProduct(data);
+          setIsLoading(false);
+        });
+    }
+  }, [id]);
+
   return (
     <Wrapper>
       <h2>Questions And Answers</h2>
       <div>
-        <QaListItem />
-        <QaListItem />
-        <QaListItem />
+        {!isLoading && currProduct.results.map((result) => (
+          <QaListItem key={result.question_id} result={result} />
+        ))}
       </div>
     </Wrapper>
   );
@@ -18,5 +36,7 @@ function QaBox() {
 export default QaBox;
 
 const Wrapper = styled.div`
-  border: 1px solid red;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  margin: 0 auto;
+  width: 70%;
 `;
