91da0c90028012aa614778256daa64253a96b6b8
Camden Smith <camdensmithh@gmail.com>
2022-07-08 17:12:20 -0700

    Added Ratings and Reviews as components to render

--- client/src/components/ratings-reviews/RrBox.jsx
+++ client/src/components/ratings-reviews/RrBox.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Reviews from './Reviews.jsx';
+import Ratings from './Ratings.jsx';
 
-function RrBox() {
+function RrBox({ id }) {
+  const [reviews, setReviews] = useState([]);
+  const [ratings, setRatings] = useState('');
+  const [count, setCount] = useState(4);
+
+  useEffect(() => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews', {
+      headers: {
+        Authorization: process.env.GITKEY,
+      },
+      params: {
+        product_id: id,
+        count,
+      },
+    })
+      .then((res) => {
+        setReviews(res.data.results);
+      })
+      .catch((err) => console.log('Error RrBox: ', err));
+  }, [id]);
+
   return (
     <div>
-      <h2>Ratings and Reviews</h2>
+      <h2>Ratings & Reviews</h2>
+      <Ratings id={id} />
+      <Reviews reviews={reviews} />
     </div>
   );
 }
