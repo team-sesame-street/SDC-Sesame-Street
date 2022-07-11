4a70a1d9708dfbff30e69c6c7e0d162388bfe685
Camden Smith <camdensmithh@gmail.com>
2022-07-11 10:10:38 -0700

    Completed reviews date component

--- client/src/components/ratings-reviews/reviews/ReviewsDate.jsx
+++ client/src/components/ratings-reviews/reviews/ReviewsDate.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import formatDate from '../../questionsAnswers/utils/formatDate';
 
 function ReviewsDate({ date }) {
-
+  return (
+    <div>
+      {formatDate(date)}
+    </div>
+  );
 }
 
 export default ReviewsDate;
