a359543c5014fdac2804bf43c93041c807693034
Camden Smith <camdensmithh@gmail.com>
2022-07-11 09:33:08 -0700

    Completed individual rating stars on review

--- client/src/components/ratings-reviews/reviews/ReviewsRatings.jsx
+++ client/src/components/ratings-reviews/reviews/ReviewsRatings.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-use-before-define */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import StarRatings from 'react-star-ratings';
 
