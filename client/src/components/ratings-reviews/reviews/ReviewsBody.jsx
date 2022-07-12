742af6aec63156f9792dac3b42c400917dd7a7d8
Camden Smith <camdensmithh@gmail.com>
2022-07-11 17:42:31 -0700

    Completed reviews body component

---
+++ client/src/components/ratings-reviews/reviews/ReviewsBody.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable no-script-url */
+/* eslint-disable react/jsx-no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Lightbox from "react-image-lightbox";
+
+function ReviewsBody({ reviewBody, reviewImages }) {
+  const [focused, setFocus] = useState(false);
+  const [imgUrl, setImgUrl] = useState('');
+  const [showMore, setShowMore] = useState(false);
+
+  function handleClick(e) {
+    const id = e.target.id;
+    if (id === 'thumbnail') {
+      setFocus(true);
+      setImgUrl(e.target.src);
+    }
+    if (id === 'more') {
+      setShowMore(true);
+    }
+  }
+
+  return (
+    <div>
+      {focused && (
+        <Lightbox
+          imageLoadErrorMessage="This image failed to load"
+          mainSrc={imgUrl}
+          onCloseRequest={() => setFocus(false)}
+        />
+      )}
+      {reviewBody.length > 250 && !showMore ? (
+        <span>
+          {reviewBody.slice(0, 249)}
+          <a href="javascript:void(0);" id="more" onClick={handleClick}>Show more</a>
+        </span>
+      ) : (
+        <span>
+          {reviewBody}
+        </span>
+      )}
+      <span style={imageContainer}>
+        {reviewImages.map((image) => (
+          <Thumbnail key={image.id} src={image.url} onClick={handleClick} id="thumbnail" />
+        ))}
+      </span>
+    </div>
+  );
+}
+
+const imageContainer = {
+  display: 'flex',
+  cursor: 'pointer',
+};
+
+const Thumbnail = styled.img`
+  aspect-ratio: 1/1;
+  object-fit: cover;
+  width: 150px;
+`;
+
+export default ReviewsBody;
