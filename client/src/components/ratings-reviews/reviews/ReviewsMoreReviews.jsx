3e4f9be981ed5bf3e5521b15149f71aaf6ece282
Camden Smith <camdensmithh@gmail.com>
2022-07-13 14:33:30 -0700

    Completed functionality of only showing more reviews button if there are more than 2 reviews

--- client/src/components/ratings-reviews/reviews/ReviewsMoreReviews.jsx
+++ client/src/components/ratings-reviews/reviews/ReviewsMoreReviews.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-function ReviewsMoreReviews({ count, setCount }) {
+function ReviewsMoreReviews({ count, setCount, totalReviews }) {
   return (
     <div className="more-reviews">
-      <button type="button" onClick={() => { setCount(count + 2); }}>MORE REVIEWS</button>
+      {totalReviews > 2 && (
+        <button type="button" onClick={() => { setCount(count + 2); }}>MORE REVIEWS</button>
+      )}
     </div>
   );
 }
