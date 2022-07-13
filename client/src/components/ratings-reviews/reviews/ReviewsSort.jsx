c41d593d5783ac21b9cdecb21e7bcac9b0a4452b
Camden Smith <camdensmithh@gmail.com>
2022-07-13 14:24:51 -0700

    Added space

--- client/src/components/ratings-reviews/reviews/ReviewsSort.jsx
+++ client/src/components/ratings-reviews/reviews/ReviewsSort.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function ReviewsSort({ setSort }) {
   return (
     <div className="sort">
-      <select onChange={() => { setSort(event.target.value)}}>
+      <select onChange={() => { setSort(event.target.value); }}>
         <option value="relevant">relevance</option>
         <option value="newest">newest</option>
         <option value="helpful">helpfulness</option>
