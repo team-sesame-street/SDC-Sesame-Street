365a91eb969bf58bd1f7247ae3d9a9226913d04f
Camden Smith <camdensmithh@gmail.com>
2022-07-13 14:54:50 -0700

    Completed More Reviews component

--- client/src/components/ratings-reviews/reviews/ReviewsMoreReviews.jsx
+++ client/src/components/ratings-reviews/reviews/ReviewsMoreReviews.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function ReviewsMoreReviews({ count, setCount, totalReviews }) {
   return (
     <div className="more-reviews">
-      {totalReviews > 2 && (
+      {(totalReviews > 2 && count < totalReviews) && (
         <button type="button" onClick={() => { setCount(count + 2); }}>MORE REVIEWS</button>
       )}
     </div>
