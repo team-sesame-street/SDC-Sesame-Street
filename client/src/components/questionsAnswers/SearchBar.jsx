276e6f5a73eca13fc1bdda08d7ed2db72dd83623
Gian Franco Lazaro <gianlazaro7411@gmail.com>
2022-07-12 18:34:26 -0700

    created a searchbar component to remove it from QABox/s logic

---
+++ client/src/components/questionsAnswers/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function SearchBar({ searchTerm, setSearchTerm }) {
+  return (
+    <SearchBarWrapper>
+      <input type="textbox" placeholder="Have a question? Search for answers…" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+    </SearchBarWrapper>
+  )
+}
+
+export default SearchBar;
+
+const SearchBarWrapper = styled.div`
+  width: 100%;
+  height: 2.5rem;
+  margin-bottom: 1rem;
+  & input {
+    border: 1px solid #333;
+    width: 100%;
+    height: 100%;
+    background: url('https://i.ibb.co/bJTc5MD/noun-search-4968922.webp') no-repeat right 10px center;
+    background-size: 25px 25px;
+    padding: 0.5rem 0.6rem;
+  }
+  & input:hover, input:focus {
+    background-color: #f1f1f1;
+  }
+`;
