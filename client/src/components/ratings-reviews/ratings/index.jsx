7b389880d322d97aa67e0132a25ea0669965969d
Camden Smith <camdensmithh@gmail.com>
2022-07-13 16:08:59 -0700

    Completed rating summary component

--- client/src/components/ratings-reviews/ratings/index.jsx
+++ client/src/components/ratings-reviews/ratings/index.jsx
@@ -1,8 +1,23 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
+import StarRatings from 'react-star-ratings';
+import getTotalRatings from '../../../../utils/getTotalRatings.js';
 
-function Ratings() {
-
+function Ratings({ meta }) {
+  let rating = 0;
+  if (meta) {
+    const {ratings} = meta;
+    const totalRatings = getTotalRatings(ratings);
+    const ratingsKeys = Object.keys(ratings).map(Number);
+    rating = ratingsKeys.reduce((total, key) => total + (
+      key * Number(ratings[key])), 0) / totalRatings;
+  }
+  const starRating = Math.round(rating * 4) / 4;
+  return (
+    <div>
+      {starRating}
+      <StarRatings rating={starRating} starDimension="18px" starSpacing="2px" starRatedColor="black" />
+    </div>
+  );
 }
 
 export default Ratings;
