0e895162dadc50000488e5e9f522bc7b207f1eb9
Camden Smith <camdensmithh@gmail.com>
2022-07-13 19:12:31 -0700

    Added Ratings Summary component

--- client/src/components/ratings-reviews/ratings/index.jsx
+++ client/src/components/ratings-reviews/ratings/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-use-before-define */
 import React from 'react';
 import StarRatings from 'react-star-ratings';
 import getTotalRatings from '../../../../utils/getTotalRatings.js';
@@ -5,19 +7,32 @@ import getTotalRatings from '../../../../utils/getTotalRatings.js';
 function Ratings({ meta }) {
   let rating = 0;
   if (meta) {
-    const {ratings} = meta;
+    const { ratings } = meta;
     const totalRatings = getTotalRatings(ratings);
     const ratingsKeys = Object.keys(ratings).map(Number);
     rating = ratingsKeys.reduce((total, key) => total + (
       key * Number(ratings[key])), 0) / totalRatings;
   }
+  const numRating = Math.round(rating * 10) / 10;
   const starRating = Math.round(rating * 4) / 4;
   return (
-    <div>
-      {starRating}
+    <div style={flexContainer}>
+      <div style={numRatingStyle}>
+        {numRating}
+      </div>
       <StarRatings rating={starRating} starDimension="18px" starSpacing="2px" starRatedColor="black" />
     </div>
   );
 }
+
+const numRatingStyle = {
+  fontSize: 60,
+  fontFamily: 'Arial',
+  fontWeight: 'bold',
+};
+
+const flexContainer = {
+  display: 'flex',
+};
 
 export default Ratings;
