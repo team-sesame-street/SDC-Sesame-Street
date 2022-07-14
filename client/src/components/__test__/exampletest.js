41c8bb9efdc49cd5a5f50f3172c715023a7b08f4
Binh <kbinhnguyen.nihongo@gmail.com>
2022-07-13 22:28:13 -0400

    Added further configurations to setup Jest suite

--- client/src/components/__test__/exampletest.js
+++ client/src/components/__test__/exampletest.js
@@ -1,26 +1,21 @@
+import React from 'react';
+import Ratings from '../relatedItems/Ratings.jsx';
+import MainCarousel from '../relatedItems/MainCarousel.jsx';
+import { render, screen, cleanup } from '@testing-library/react';
+import { toBeInTheDocument } from '@testing-library/jest-dom';
 
-/**
- * @jest-environment jsdom
- */
+afterEach(() => {
+  cleanup();
+});
 
- import React from 'react'
- import Ratings from '../relatedItems/Ratings.jsx'
- import MainCarousel from '../relatedItems/MainCarousel.jsx'
- import { render, screen, cleanup } from '@testing-library/react'
- import { toBeInTheDocument } from '@testing-library/jest-dom';
+test('This should render product carousel component', () => {
+  render(<Ratings rating={3} />);
+  const element = screen.getByTestId('ratings');
+  expect(element).toBeInTheDocument();
+});
 
- afterEach(() => {
-   cleanup()
- })
-
- test('This should render product carousel component', () => {
-   render(<Ratings rating={3}/>)
-   const element = screen.getByTestId('ratings')
-   expect(element).toBeInTheDocument()
- });
-
- test('This should render the main carousel component', () => {
-   render(<MainCarousel id={40346}/>)
-   const element = screen.getByTestId('main')
-   expect(element).toBeInTheDocument()
- });
+test('This should render the main carousel component', () => {
+  render(<MainCarousel id={40346} />);
+  const element = screen.getByTestId('main');
+  expect(element).toBeInTheDocument();
+});
