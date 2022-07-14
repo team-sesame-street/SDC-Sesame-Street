ba57b48a3e3155a3d3ab35699e32fb2a3429e14e
Camden Smith <camdensmithh@gmail.com>
2022-07-13 23:06:06 -0700

    Added RatingsBreakdown to render

--- client/src/components/ratings-reviews/ratings/index.jsx
+++ client/src/components/ratings-reviews/ratings/index.jsx
@@ -3,12 +3,15 @@
 import React from 'react';
 import StarRatings from 'react-star-ratings';
 import getTotalRatings from '../../../../utils/getTotalRatings.js';
+import RatingsBreakdown from './RatingsBreakdown.jsx';
 
 function Ratings({ meta }) {
   let rating = 0;
+  let totalRatings = 0;
+  let ratings = {};
   if (meta) {
-    const { ratings } = meta;
-    const totalRatings = getTotalRatings(ratings);
+    ratings = meta.ratings;
+    totalRatings = getTotalRatings(ratings);
     const ratingsKeys = Object.keys(ratings).map(Number);
     rating = ratingsKeys.reduce((total, key) => total + (
       key * Number(ratings[key])), 0) / totalRatings;
@@ -16,14 +19,30 @@ function Ratings({ meta }) {
   const numRating = Math.round(rating * 10) / 10;
   const starRating = Math.round(rating * 4) / 4;
   return (
-    <div style={flexContainer}>
-      <div style={numRatingStyle}>
-        {numRating}
+    <div style={ratingsContainer}>
+      <div style={flexContainer}>
+        <div style={numRatingStyle}>
+          {numRating}
+        </div>
+        <StarRatings rating={starRating} starDimension="18px" starSpacing="2px" starRatedColor="black" />
       </div>
-      <StarRatings rating={starRating} starDimension="18px" starSpacing="2px" starRatedColor="black" />
+      <div style={breakdownContainer}>
+        <RatingsBreakdown ratings={ratings} totalRatings={totalRatings}
+        onClick={() => {console.log(event.target)}}/>
+      </div>
     </div>
   );
 }
+
+const ratingsContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '30px',
+};
+
+const flexContainer = {
+  display: 'flex',
+};
 
 const numRatingStyle = {
   fontSize: 60,
@@ -31,8 +50,9 @@ const numRatingStyle = {
   fontWeight: 'bold',
 };
 
-const flexContainer = {
+const breakdownContainer = {
   display: 'flex',
+  flexDirection: 'column',
 };
 
 export default Ratings;
