a0b15b9df9bbafe9bf19c89e9ef8ba7f7d731c29
Gian Franco Lazaro <gianlazaro7411@gmail.com>
2022-07-16 19:06:39 -0700

    A loading circle for my components loading state

---
+++ client/utils/LoadingCircle.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { VscLoading } from "react-icons/vsc";
+
+function LoadingCircle() {
+  return (
+    <Wrapper>
+      <VscLoading/>
+    </Wrapper>
+  );
+}
+
+export default LoadingCircle;
+
+const rotatingAnimation = keyframes`
+  0% {transform: rotate(0deg);}
+  100% {transform: rotate(360deg);}
+`;
+
+const Wrapper = styled.div`
+  & * {
+    margin: 20% auto;
+    animation: ${rotatingAnimation} 1s ease-in-out infinite;
+    transform-origin: center;
+    width: 30px;
+    height: 30px;
+  }
+`;
