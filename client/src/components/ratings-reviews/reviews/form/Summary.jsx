6de0a4fa830dd8367b95b2fad1c590c1fcb925c1
Camden Smith <camdensmithh@gmail.com>
2022-07-17 20:12:58 -0700

    1.2.6.4 Completed. Added new review summary section and implemented.

---
+++ client/src/components/ratings-reviews/reviews/form/Summary.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const styles = {
+  warning: {
+    fontStyle: 'italic',
+    fontSize: '12px',
+  },
+};
+
+export default function Summary({ summaryText, setSummaryText }) {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Example: Best purchase ever!"
+        size="60"
+        maxLength="60"
+        onChange={(event) => setSummaryText(event.target.value)}
+        value={summaryText}
+      />
+      <br />
+      <div style={styles.warning}>
+        {`${60 - summaryText.length} characters remaining`}
+      </div>
+    </div>
+  );
+}
