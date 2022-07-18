e2cdc9d7a2387224aefd1c79f604af56c73816ff
Camden Smith <camdensmithh@gmail.com>
2022-07-17 19:13:43 -0700

    Implementation of Characteristics portion of form modal

--- client/src/components/ratings-reviews/reviews/form/Modal.jsx
+++ client/src/components/ratings-reviews/reviews/form/Modal.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import Rating from './Rating.jsx';
+import Characteristics from './Characteristics.jsx';
 
 const styles = {
-  modal: {
+  modalContainer: {
     position: 'fixed',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%',
     backgroundColor: '#fff',
     padding: '50px',
+    height: '75%',
+    width: '50%',
     zIndex: 1000,
   },
   modalOverlay: {
@@ -20,6 +23,12 @@ const styles = {
     bottom: 0,
     backgroundColor: 'rgba(0, 0, 0, .7',
     zIndex: 1000,
+  },
+  modalItems: {
+    overflow: 'scroll',
+    maxWidth: '100%',
+    maxHeight: '40vh',
+    padding: '2rem',
   },
   xReviewBtn: {
     marginTop: '2px',
@@ -42,6 +51,14 @@ export default function Modal({ showModal, onClose }) {
 
   const [starRating, setStarRating] = useState();
   const [recommendProduct, setRecommendProduct] = useState();
+  const [descriptionRate, setDescriptionRate] = useState({
+    Comfort: null,
+    Fit: null,
+    Length: null,
+    Size: null,
+    Quality: null,
+    Width: null,
+  });
 
   const ratingType = ['Poor', 'Fair', 'Average', 'Good', 'Great'];
 
@@ -52,7 +69,7 @@ export default function Modal({ showModal, onClose }) {
   return (
     <>
       <div style={styles.modalOverlay} />
-      <div style={styles.modal}>
+      <div style={styles.modalContainer}>
         <form>
           <button type="button" onClick={onClose} style={styles.xReviewBtn}>X</button>
           <br />
@@ -62,39 +79,53 @@ export default function Modal({ showModal, onClose }) {
             {`About the ${localStorage.getItem('productName')}`}
           </h2>
           <br />
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label>Overall Rating*</label>
-          <Rating
-            starRating={starRating}
-            changeRating={(userRating) => setStarRating(userRating)}
-          />
-          {ratingType[starRating - 1]}
-          <br />
-          Do You Recommend This Product?*
-          <label>
-            <input
-              type="radio"
-              value="yes"
-              checked={recommendProduct === 'yes'}
-              onChange={() => { setRecommendProduct('yes'); }}
+          <div style={styles.modalItems}>
+            <b>Overall Rating</b>
+            *
+            <br />
+            <Rating
+              starRating={starRating}
+              changeRating={(userRating) => setStarRating(userRating)}
             />
-            yes
-          </label>
-          <label>
-            <input
-              type="radio"
-              value="no"
-              checked={recommendProduct === 'no'}
-              onChange={() => { setRecommendProduct('no'); }}
+            {' '}
+            {ratingType[starRating - 1]}
+            <br />
+            <b>Do You Recommend This Product?</b>
+            *
+            <br />
+            <label>
+              <input
+                type="radio"
+                value="yes"
+                checked={recommendProduct === 'yes'}
+                onChange={() => { setRecommendProduct('yes'); }}
+              />
+              {' '}
+              Yes
+            </label>
+            {' '}
+            <label>
+              <input
+                type="radio"
+                value="no"
+                checked={recommendProduct === 'no'}
+                onChange={() => { setRecommendProduct('no'); }}
+              />
+              {' '}
+              No
+            </label>
+            <br />
+            <b>Characteristics</b>
+            *
+            <Characteristics
+              descriptionRate={descriptionRate}
+              setDescriptionRate={(value) => { setDescriptionRate(value); }}
             />
-            no
-          </label>
-          <br />
-          Characteristics *
-          <input type="text" />
+          </div>
           <br />
           <input type="button" value="Cancel" onClick={onClose} style={styles.cancelReviewBtn} />
           <input type="submit" style={styles.submitReviewBtn} value="Submit" />
+
         </form>
       </div>
     </>
