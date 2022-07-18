4c7e8e114d31adfa888767f9d91513677dbbe4e5
Gian Franco Lazaro <gianlazaro7411@gmail.com>
2022-07-18 11:00:36 -0700

    Removed the styles from the subcomponent

--- client/src/components/questionsAnswers/QaBox.jsx
+++ client/src/components/questionsAnswers/QaBox.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
 import SearchBar from './SearchBar.jsx';
 import QAWrapper from './QAWrapper.jsx';
+import { Wrapper, PrimaryBtnGroup } from './styles/qabox.styles';
 
 function QaBox({ id }) {
   const [questions, setQuestions] = useState([]);
@@ -134,61 +134,3 @@ function QaBox({ id }) {
 }
 
 export default QaBox;
-
-const Wrapper = styled.div`
-  isolation: isolate;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  margin: 50px auto 0 auto;
-  padding-bottom: 4rem;
-  width: 50%;
-  @media (max-width: 500px) {
-    margin: 25px 0 0 0;
-    width: 100%;
-    padding: 15px;
-  }
-
-  & .qa-component-header {
-    margin: 1.25rem 0;
-
-    @media(max-width:500px) {
-      width: 100%;
-      text-align: center;
-      padding: 0 10px;
-    }
-  }
-`;
-
-
-const PrimaryBtnGroup = styled.div`
-  display: flex;
-
-  & .QAButton {
-    height: 3rem;
-    padding: 0 1rem;
-    justify-content: flex-start;
-    text-transform: uppercase;
-    font-weight: 700;
-    background: none;
-    border: 1px solid #222;
-    margin-right: 10px;
-    margin-top: 15px;
-    &:hover {
-      color: #eee;
-      background: #222;
-    }
-    &:disabled {
-      background: grey;
-      color: white;
-      opacity: 0.25;
-    }
-
-    @media(max-width: 500px) {
-      flex: 1;
-      padding: 0;
-      height: 5rem;
-    }
-  }
-  @media(max-width: 500px) {
-    justify-content: space-between;
-  }
-`;
