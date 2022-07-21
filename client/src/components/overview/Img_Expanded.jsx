4820944625464616e5e5b2f86fe6c5838ecc0b90
Binh <kbinhnguyen.nihongo@gmail.com>
2022-07-21 12:50:02 -0400

    Zoomed in image progress - can zoom in at position relative to original click but not the correct aspect ratio yet

--- client/src/components/overview/Img_Expanded.jsx
+++ client/src/components/overview/Img_Expanded.jsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { IoIosArrowDropright, IoIosArrowDropleft } from 'react-icons/io';
 import { IoExitOutline } from 'react-icons/io5';
 import { BsCircleFill } from 'react-icons/bs';
 import styled from 'styled-components';
+// import Zoom from './Img_Zoomed.jsx';
 
 function ExpandedImage({
   images, currImgIndex, setCurrImgIndex, setExpandedView,
 }) {
+  const [zoom, setZoom] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const getMouseClickPosition = (e) => {
+    console.log('I was called');
+    setOffset({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
+  };
+
   if (images.length > 0) {
     return (
-      <div>
+      <ExtraWrapper>
+        {/* {zoom && (
+          <Zoom imageURL={images[currImgIndex].url} setZoom={setZoom} offsetPosition={offset} />
+        )} */}
+        {console.log(offset)}
         {images.map((image, index) => {
           if (index === currImgIndex) {
             return (
-              <Wrapper key={index}>
-                {index > 0 && (
+              <Wrapper key={index} onClick={getMouseClickPosition}>
+                {!zoom && index > 0 && (
                   <IoIosArrowDropleft
                     className="icon-expanded left-arrow-expanded"
                     onClick={() => { setCurrImgIndex(currImgIndex - 1); }}
                   />
                 )}
-                <Image src={images[currImgIndex].url} alt="A representation of this product" loading="lazy" />
+                {zoom && (
+                  <ZoomImg
+                    src={images[currImgIndex].url}
+                    alt="A zoomed in perspective"
+                    onClick={() => { setZoom(false); }}
+                    style={{ objectPosition: `-${offset.x}px -${offset.y}px`}}
+                  />
+                )}
+                {!zoom && (
+                  <Image
+                    src={images[currImgIndex].url}
+                    alt="A representation of this product"
+                    loading="lazy"
+                    onClick={() => { setZoom(true); }}
+                  />
+                )}
                 <IoExitOutline data-testid="exit-expanded-btn" className="icon-expanded exit-icon" onClick={() => { setExpandedView(false); }} />
-                {index < images.length - 1 && (
+                {!zoom && index < images.length - 1 && (
                   <IoIosArrowDropright
                     className="icon-expanded right-arrow-expanded"
                     onClick={() => { setCurrImgIndex(currImgIndex + 1); }}
@@ -34,30 +62,33 @@ function ExpandedImage({
           }
           return null;
         })}
-        <NavSymbols>
-          {images.map((image, index) => {
-            const circleStyle = {
-              width: index === currImgIndex ? '11px' : '8px',
-              height: index === currImgIndex ? '11px' : '8px',
-            };
-            return (
-              <BsCircleFill
-                data-testid="nav-symbols-circles"
-                className="nav-symbols-circles"
-                key={index}
-                style={circleStyle}
-                onClick={() => {
-                  if (index !== currImgIndex) {
-                    setCurrImgIndex(index);
-                  }
-                }}
-              />
-            );
-          })}
-        </NavSymbols>
-      </div>
+        {!zoom && (
+          <NavSymbols>
+            {images.map((image, index) => {
+              const circleStyle = {
+                width: index === currImgIndex ? '11px' : '8px',
+                height: index === currImgIndex ? '11px' : '8px',
+              };
+              return (
+                <BsCircleFill
+                  data-testid="nav-symbols-circles"
+                  className="nav-symbols-circles"
+                  key={index}
+                  style={circleStyle}
+                  onClick={() => {
+                    if (index !== currImgIndex) {
+                      setCurrImgIndex(index);
+                    }
+                  }}
+                />
+              );
+            })}
+          </NavSymbols>
+        )}
+      </ExtraWrapper>
     );
   }
+  return null;
 }
 
 ExpandedImage.propTypes = {
@@ -72,14 +103,23 @@ ExpandedImage.propTypes = {
 
 export default ExpandedImage;
 
+const ExtraWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 const Wrapper = styled.div`
+  background-color: grey;
   position: relative;
   isolation: isolate;
   margin: auto;
-  width: 90vw;
+  width: 60vw;
   height: 60vw;
+  // width: 90%;
+  // height: 90%;
   max-height: 80vh;
   max-width: 80vh;
+  overflow: hidden;
   & .icon-expanded {
     position: absolute;
     z-index: 100;
@@ -104,6 +144,9 @@ const Wrapper = styled.div`
     top: 50%;
     right: 5%;
   };
+  @media(max-width: 500px) {
+    width: 100%;
+  }
 `;
 
 const NavSymbols = styled.div`
@@ -129,6 +172,18 @@ const Image = styled.img`
   position: absolute;
   width: 100%;
   height: 100%;
+  // width: auto;
+  // height: auto;
   cursor: crosshair;
   object-fit: contain;
+`;
+
+const ZoomImg = styled.img`
+  position: absolute;
+  // width: 250%;
+  // height: 250%;
+  object-fit: cover;
+  transform: scale(2.5);
+  top: 0;
+  left: 0;
 `;
