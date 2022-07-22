b4413d9e76ce2cc276cda07ead4619ba8787e325
Binh <kbinhnguyen.nihongo@gmail.com>
2022-07-22 19:16:32 -0400

    Removed duplication of photos, description, and features

--- client/src/components/overview/SloganDescription.jsx
+++ client/src/components/overview/SloganDescription.jsx
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 function SloganDescription({ product }) {
   if (Object.keys(product).length > 0) {
     // copying the features list and description to create dummy data for increased text length
-    const description = [product.description, product.description, product.description].join(' ');
+    // const description = [product.description, product.description, product.description].join(' ');
     return (
       <Wrapper>
         <Slogan data-testid="slogan">{product.slogan}</Slogan>
-            {/* <p test-data-id="description">{product.description}</p> */}
         <br />
-        <p data-testid="description">{description}</p>
+        {/* <p data-testid="description">{description}</p> */}
+        <p test-data-id="description">{product.description}</p>
       </Wrapper>
     );
   }
